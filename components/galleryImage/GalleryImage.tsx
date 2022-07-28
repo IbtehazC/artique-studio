c4e6cfb793e8d89899353dec6202350ac82a5e5c
ibtehazc <ibtehaz.chowdhury@gmail.com>
2022-07-28 11:32:09 +0600

    V2

--- components/galleryImage/GalleryImage.tsx
+++ components/galleryImage/GalleryImage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Image from "next/image";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import EditForm from "../editForm/EditForm";
 
 interface Props {
@@ -14,17 +14,15 @@ export default function GalleryImage({ image, admin }: Props) {
   return (
     <a href="#" className="group">
       <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden bg-gray-200">
-        <Image
+        <LazyLoadImage
           src={`${image.imgSrc}`}
           alt="placeholder"
-          layout="fill"
-          objectFit="cover"
           className={`group-hover:opacity-75 duration-700 ease-in-out ${
             loading
               ? "grayscale blur-2xl scale-110"
               : "grayscale-0 blur-0 scale-100"
           }`}
-          onLoadingComplete={() => setLoading(false)}
+          afterLoad={() => setLoading(false)}
         />
       </div>
       {admin && (
@@ -47,7 +45,9 @@ export default function GalleryImage({ image, admin }: Props) {
           </button>
         </div>
       )}
-      {editMode && <EditForm title={image.title} description={image.description} />}
+      {editMode && (
+        <EditForm title={image.title} description={image.description} />
+      )}
     </a>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ["vmpkfzmgcwrtqhrhzfmb.supabase.co"],
-    unoptimized: true,
-  },
   env: {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
